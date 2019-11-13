dde0872dc320c8786217a668062a325213a4ac93
yanqw <271853976@qq.com>
2019-11-13 14:49:26 +0800

    Signed-off-by: yanqw <271853976@qq.com>

--- DataCleaner-DataCleaner-5.1.0/components/visualization/src/main/scala/org/datacleaner/visualization/VisualizationCategory.scala
+++
@@ -1,9 +0,0 @@
-package org.datacleaner.visualization
-
-import org.datacleaner.api.ComponentCategory
-import org.datacleaner.components.categories.AbstractComponentCategory
-
-class VisualizationCategory extends AbstractComponentCategory {
-
-  override def getName() = "Visualization"
-}
